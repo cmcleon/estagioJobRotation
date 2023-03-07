c569c0f402c2a0e1679a9a2cd7310619d3377053
cmcleon <candicem.leon@gmail.com>
2023-03-06 23:43:56 -0300

    :up: resposta 3

--- respostas/resposta1.py
+++ respostas/resposta1.py
@@ -1,13 +1,12 @@
 #RESPOTA 1
 
 Indice = 13
-
 Soma = 0
 
-K = 0
+k = 0
 
-while K < Indice:
-   K = K + 1
-   Soma = Soma + K
+while k < Indice:
+   k = k + 1
+   Soma = Soma + k
 
 print ('Soma =', Soma)
--- respostas/resposta3.py
+++ respostas/resposta3.py
@@ -7,3 +7,33 @@
 #a) Usar o json ou xml disponível como fonte dos dados do faturamento mensal;
 #b) Podem existir dias sem faturamento, como nos finais de semana e feriados. Estes dias devem ser ignorados no cálculo da média;
 
+import json
+
+with open('dados.json') as json_file:
+    dados = json.load(json_file)
+
+    minV = dados[0]
+    maxV = dados[0]
+    somaMedia = 0.0
+    numDiasSUperiorFaturamento = 0
+
+    #print(len(dados))
+    for i in dados:
+        if(i['valor'] != 0):
+            somaMedia = somaMedia + i['valor']
+            if(i['valor'] > maxV['valor']):
+                maxV = i
+            elif (i['valor'] < minV['valor']):
+                minV = i
+    
+    media = (somaMedia/len(dados))
+    
+    for j in dados:
+        if(j['valor'] != 0):
+            if(j['valor'] > media):
+                numDiasSUperiorFaturamento += 1 
+    
+    
+    print('Menor valor de faturamento: ',minV['valor'])
+    print('Maior valor de faturamento: ',maxV['valor'])
+    print('Número de dias no mês em que o valor de faturamento diário foi superior à média mensal: ',numDiasSUperiorFaturamento)
